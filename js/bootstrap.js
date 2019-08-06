b8d773727049e732f38c2b615a680b24887df00e
matthewso2 <49337222+matthewso2@users.noreply.github.com>
2019-08-06 13:03:27 -0400

    changes made

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,5 +1,7 @@
 
-$('.dropdown-menu a.dropdown-item').on('click', function(e) {
+// DROPDOWN MENU DOESNT DISAPPEAR AFTER CLICK SCRIPT
+
+$('.click-stay').on('click', function(e) {
     if (!$(this).next().hasClass('show')) {
       $(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
     }
@@ -15,13 +17,98 @@ $('.dropdown-menu a.dropdown-item').on('click', function(e) {
     return false;
   });
 
-$('.eqp-grp').hide()
-$('#equip-products').click(function(){
+// MENU SLIDE SELECTED SCRIPT
+ 
+$('#equip-products').on('click', function(e){
 
-    const old = $('.eqp-grp').show();
-    const left = $('.dropdown-item').hide();
-    
-    
-    //  const old = $('.eqp-grp').animate({ right: '251.14px'});
-    //  const left = $('.dropdown-item').animate({ right: '251.14px' });
+    $(".eqp-grp").css("display", "block");
+    $('#ep-sub').show().animate({
+        right: '100%',
+        width: '130%'
+    }, 150);
+
+    $('.dropdown-item').delay(150).hide();
+});
+
+
+$('#expertise').click(function(){
+
+  $(".ex-grp").css("display", "block");
+  $('#ex-sub').show().animate({
+      right: '100%',
+      width: '130%'
+  }, 150);
+
+  $('.dropdown-item').delay(150).hide();
+  
+});
+
+$('#human-resources').click(function(){
+
+  $(".hr-grp").css("display", "block");
+  $('#hr-sub').show().animate({
+      right: '100%',
+      width: '130%'
+  }, 150);
+
+  $('.dropdown-item').delay(150).hide();
+  
+});
+
+$('#real-estate').click(function(){
+
+  $(".re-grp").css("display", "block");
+  $('#re-sub').show().animate({
+      right: '100%',
+      width: '130%'
+  }, 150);
+
+  $('.dropdown-item').delay(150).hide();
+  
+});
+
+// BACK MENU SCRIPT
+
+$('#eqp-back').on('click', function(e) {
+
+  $(".dropdown-item").css("display", "block");
+  $('.dropdown-item').show().animate({
+      left: '100%',
+      width: '100%'
+  }, 150);
+
+  $('#ep-sub').delay(150).hide();
+});
+
+$('#exp-back').click(function(){
+
+  $(".dropdown-item").css("display", "block");
+  $('.dropdown-item').show().animate({
+      left: '100%',
+      width: '100%'
+  }, 150);
+
+  $('#ex-sub').delay(150).hide();
+});
+
+$('#hr-back').click(function(){
+
+  $(".dropdown-item").css("display", "block");
+  $('.dropdown-item').show().animate({
+      left: '100%',
+      width: '100%'
+  }, 150);
+
+  $('#hr-sub').delay(150).hide();
+});
+
+$('#re-back').click(function(){
+
+  $(".dropdown-item").css("display", "block");
+  $('.dropdown-item').show().animate({
+      left: '100%',
+      width: '100%'
+  }, 150);
+
+  $('#re-sub').delay(150).hide();
 });
