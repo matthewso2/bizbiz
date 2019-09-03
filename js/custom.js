1c06e4251167d603f7f1c9e3944840fb24fa22a6
matthewso2 <49337222+matthewso2@users.noreply.github.com>
2019-09-03 08:26:07 -0400

    cleaned it up

--- js/custom.js
+++ js/custom.js
@@ -1,41 +1,22 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("mainDropdown").classList.toggle("show");
-  // document.getElementsByClassName("submenu").classList.toggle("show");
-  console.log(document.getElementsByClassName('submenu'));
+
+
+function toggleMenu() {
+  $('.panel').hide(); // Hide all panels
+  $('#panel1').show(); // Show 1st panel
+  $('#menu').toggle();
 }
 
-// Close the dropdown menu if the user clicks outside of it
-// window.onclick = function(event) {
-//   if (!event.target.matches('.dropbtn')) {
-//     var dropdowns = document.getElementsByClassName("dropdown-content");
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//       }
-//     }
-//   }
-// }
+function toggleSubMenu(_this, show, reverse) {
+  if (reverse == true) {
+      $(_this).parent().hide("slide", { direction: "right" }, 500);
+      $(show).show("slide", { direction: "left" }, 500);
+  } else {
+      $(_this).parent().hide("slide", { direction: "left" }, 500);
+      $(show).show("slide", { direction: "right" }, 500);
+  }
+}
 
-// Selected animation script
-
-function menuAnimation(showElement, hideElement) {
-  $(showElement).css('width', 'unset');
-  $(showElement).css('right', 'unset');
-  $(showElement).css('left', 'unset');
-	$(showElement).css({visibility: "visible"}).animate({
-		right: '100%',
-		width: '50%'
-  }, 150);
-
-  $(hideElement).css('width', 'unset');
-  $(hideElement).css('right', 'unset');
-  $(hideElement).css('left', 'unset');
-  $(hideElement).animate({
-  right: '100%',
-  width: '100%'
-	}, 150).css({visibility: "hidden"}, 150);
+function displayContent(_this) {
+  var elementText = $(_this).text();
+  $('#content').text(elementText);
 }
