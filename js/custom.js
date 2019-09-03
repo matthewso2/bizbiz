5ca6478257d84e8daab7c8771d3f7e6834d18776
matthewso2 <49337222+matthewso2@users.noreply.github.com>
2019-09-03 12:54:14 -0400

    test cose completed

--- js/custom.js
+++ js/custom.js
@@ -11,10 +11,12 @@ function toggleSubMenu(_this, show, reverse, _content) {
   if (reverse == true) {
       $(_this).parent().hide("slide", { direction: "right" }, 500);
       $(show).show("slide", { direction: "left" }, 500);
+      $(".contents").show("slide", { direction: "left" }, 500);
   } else {
       $(_this).parent().hide("slide", { direction: "left" }, 500);
       $(show).show("slide", { direction: "right" }, 500);
-      $(_content).show("slide", { direction: "left" }, 500);
+
+      $(".contents").not(_content).hide("slide", { direction: "right" }, 500);
   }
 }
 
